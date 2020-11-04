b4ec500d11a77029bcd564e9ee3b41a7f91647c3
ppupo <ppupo@talenta.cloud>
2020-11-04 11:43:56 -0500

    Structure server

---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+export class Server {
+  private port: number;
+
+  constructor(port: number) {
+    this.port = port;
+  }
+
+  static init(port: number) {
+    return new Server(port);
+  }
+
+  start(callback: () => void) {}
+
+  private async connect() {}
+}
