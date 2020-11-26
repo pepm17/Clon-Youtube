a48ab5e4e7417f096ac8d4e8aa5f2352857e4d66
ppupo <ppupo@talenta.cloud>
2020-11-25 20:53:03 -0500

    Traduction and validation esp

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,7 +5,6 @@ import morgan from "morgan";
 import { LOG_FORMAT } from "../constants/environments.constant";
 import { stream } from "../utils/logger/logger.util";
 import { createConnection } from "typeorm";
-import cors from "cors";
 export default class Server {
   private port: number;
 
@@ -25,7 +24,6 @@ export default class Server {
           swaggerUi.serve,
           swaggerUi.setup(specs.default, { explorer: true })
         );
-        app.use(cors());
         app.use(morgan(LOG_FORMAT, { stream }));
         app.listen(this.port, callback);
       })
