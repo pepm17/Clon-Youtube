937061eccb074ab5fc7b1a835b4895d6ead10cdf
pepm17 <pablopupo1797@gmail.com>
2020-12-07 19:12:46 -0500

    get allvideo with redux

--- client/src/video/Content.tsx
+++ client/src/video/Content.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { VideoGrid } from './VideoGrid'
 import './Content.css'
+import { useDispatch } from 'react-redux'
+import { getAllVideos } from './video.redux'
 
 export const Content = () => {
-  const grids = Array.apply(null, new Array(2)).map((val, index) => (
+  const grids = Array.apply(null, new Array(1)).map((val, index) => (
     <VideoGrid key={index} title="Title" />
   ))
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getAllVideos())
+  },[dispatch])
 
   return (
     <div className="video_content">
--- client/src/video/VideoGrid.tsx
+++ client/src/video/VideoGrid.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { VideoPreview } from './VideoPreview'
 import './VideoGrid.css'
 import { VideoGridHeader } from './VideoGridHeader'
 import { Divider } from 'semantic-ui-react'
 import { IVideoGridHeader } from './video.interface'
-import { getAllVideos } from './video.service'
-import { IVideo } from './video.interface'
+// import { getAllVideos } from './video.redux'
+import { useSelector } from 'react-redux'
+import { RootStore } from "../redux/store"
 
 export const VideoGrid = (props: IVideoGridHeader) => {
-  const [videos, setVideos] = useState<IVideo[]>([])
-  let preview = videos.map((val, index) => <VideoPreview {...val} key={index} />)
-  useEffect(() => {
-    fetchAllVideos()
-  }, [])
-
-  const fetchAllVideos = async () => {
-    const response = await getAllVideos()
-    setVideos(response)
-  }
-
+  const videoState = useSelector((state: RootStore) => state.videos)
+  let preview = videoState.videos.map((val, index) => <VideoPreview {...val} key={index} />)
+  
   return (
     <>
       <VideoGridHeader title={props.title} />
