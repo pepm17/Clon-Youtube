2b78be7641f332d401336c982374323c0ce9b74b
pepm17 <pablopupo1797@gmail.com>
2020-12-07 19:11:37 -0500

    use store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store = {store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
