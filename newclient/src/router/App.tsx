0b4b0274aeb37b36b4392b9ee84edce988372f63
Pablo Emilio <pablopupo1797@gmail.com>
2021-04-20 08:49:14 -0500

    refactor: add responsive
    add responsive in .scss

--- newclient/src/router/App.tsx
+++ newclient/src/router/App.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/header";
+import SideBar from "../components/sidebar";
 import Home from "../pages/home";
 import NotFound from "../pages/notFound";
 import "./App.scss";
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Header />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </>
+      <div className="app_content">
+        <div className="app_sidebar">
+          <SideBar />
+        </div>
+        <div className="app_content_content">
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
   );
 };
 
