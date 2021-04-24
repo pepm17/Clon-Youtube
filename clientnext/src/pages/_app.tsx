cfef2aeeb82b21ac0b6e85036255c4aabbe307ad
Pablo Emilio <pablopupo1797@gmail.com>
2021-04-23 23:52:39 -0500

    feat: add _app with styles and page home

---
+++ clientnext/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from "next/app";
+import Header from "../components/header/header";
+import Head from "next/head";
+import style from "./_app.module.scss";
+import "./_app.scss";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={style.app}>
+        <Header />
+        <div className={style.app_content}>
+          <div className={style.app_sidebar}></div>
+          <div className={style.app_content_content}>
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
