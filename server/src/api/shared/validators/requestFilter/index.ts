d74608b3d5c7d35d4a8205931ef4b30d480eaa87
Pablo Emilio <pablopupo1797@gmail.com>
2021-05-25 15:55:58 -0500

    refactor: delete validators

--- server/src/api/shared/validators/requestFilter/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./loginFilter.validator";
-export * from "./registerFilter.validator";
-export * from "./findById.validator";
-export * from "./videoCreate.validator";
--- server/src/api/shared/validators/requestFilter/loginFilter.validator.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from "class-validator";
-
-export class LoginFilterValidator {
-  @IsString({ message: "validator:isString" })
-  username!: string;
-  @IsString({ message: "validator:isString" })
-  password!: string;
-}
